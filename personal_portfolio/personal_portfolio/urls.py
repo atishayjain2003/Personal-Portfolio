18c36bc74b8597dd09b6e5936093aaf403f6cd00
atishayjain2003 <atishayjain2003@gmail.com>
2024-05-04 23:59:46 +0530

    Adding image root

--- personal_portfolio/personal_portfolio/urls.py
+++ personal_portfolio/personal_portfolio/urls.py
@@ -17,8 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from portfolio import views
+from django.conf.urls import static
+from django.conf import settings 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name='home' )
 ]
+
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
